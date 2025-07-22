0db4b1170a640e34f1416a7fc5a75a8026628630
MrsMillerComputing <felicity.miller@ab-ed.org>
2025-07-22 21:57:13 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+# Task: Create a fruit stand receipt program
+
+# 1. Print a welcome message for the user
+# (Must include the word "welcome")
+
+
+
+# 2. Ask the user how many fruits they are buying
+
+
+
+# 3. Use a running total within a loop to calculate the total price of the fruits
+
+
+
+# 4. Display the final message using concatenation
+# It must include the words: Your total is £
+# Example: Your total is £3.45. Thank you for shopping with us!
+
+
+
